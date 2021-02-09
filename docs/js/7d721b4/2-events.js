5b4a4279c1fb6560a60acd0959ee426590af4d48
luimu64 <54173370+luimu64@users.noreply.github.com>
2021-02-09 15:45:15 +0200

    Delete docs directory

--- docs/js/7d721b4/2-events.js
+++
@@ -1,1 +0,0 @@
-Sydney.events=function(){return{trigger:function(a,b,c){$(document).trigger("sydney-"+a+"-"+b,c)},on:function(a,b,c){$(document).on("sydney-"+a+"-"+b,c)},off:function(a,b,c){_.isUndefined(c)?$(document).off("sydney-"+a+"-"+b):$(document).off("sydney-"+a+"-"+b,c)}}}();
